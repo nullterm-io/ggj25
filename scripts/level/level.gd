18b474522820a80e4018b02fb8caf783525e1617
Ivan Nikolaev <voidexp@gmail.com>
2025-01-25 17:13:45 +0200

    Angle-based variation of the generated level

--- scripts/level/level.gd
+++ scripts/level/level.gd
@@ -3,6 +3,9 @@ extends Node3D
 ## Number of torus segments at one time
 @export var pipe_length = 50
 
+## Distance after which the curvature angle changes
+@export var angle_change_dist = 25
+
 ## Maximum random offset between segments
 @export var max_offset = 0.01
 
@@ -14,6 +17,8 @@ extends Node3D
 
 @onready var _segment_length = (section_template.outer_radius - section_template.inner_radius)
 
+var _elapsed_distance := 0.0
+var _angle := 0.0
 var _segments: Array = []
 
 func _ready():
@@ -27,6 +32,20 @@ func _ready():
 	section_template.visible = false
 
 func _physics_process(delta: float) -> void:
+
+	_update_angle(delta)
+	_update_sections(delta)
+
+func _process(delta: float) -> void:
+	_update_position(delta)
+
+func _update_angle(delta: float) -> void:
+	_elapsed_distance += scroll_speed * delta
+	if _elapsed_distance >= angle_change_dist:
+		_angle = randf() * TAU
+		_elapsed_distance -= angle_change_dist
+
+func _update_sections(delta: float) -> void:
 	var segments_to_erase = 0
 	var run_length = 0.0
 
@@ -42,9 +61,10 @@ func _physics_process(delta: float) -> void:
 	# Add new segments to cover the remaining run length
 	while run_length < pipe_length:
 		var segment = _segments[-1].duplicate()
+		var offset = Vector3(cos(_angle), sin(_angle), 0) * _segment_length * max_offset
 		_segments.append(segment)
 		add_child(segment)
-		segment.position += Vector3.FORWARD * _segment_length
+		segment.position += Vector3.FORWARD * _segment_length + offset
 		run_length += _segment_length
 
 	# Erase segments past zero
@@ -52,9 +72,7 @@ func _physics_process(delta: float) -> void:
 		_segments.pop_front().queue_free()
 		segments_to_erase -= 1
 
-func generate_offset_vector(z: float) -> Vector3:
-	return Vector3(
-		randf_range(-max_offset, max_offset),
-		randf_range(-max_offset, max_offset),
-		z,
-	)
+func _update_position(_delta: float) -> void:
+	var forward_segment = _segments[0]
+	global_position = -forward_segment.position
+	global_position.z = 0
