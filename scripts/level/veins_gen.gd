ff174528b6d7c12c62f385c96d67050a8a601240
snatvb <snatvb@ya.ru>
2025-01-25 22:22:07 +0200

    A bit refactoring

--- scripts/level/veins_gen.gd
+++ scripts/level/veins_gen.gd
@@ -21,7 +21,7 @@ func _init(r: float, max_obstacles: int):
 func _calc_arc_length_between_points(a: Vector2, b: Vector2) -> float:
 	# NOTE: assume that a and b are points both on circle, meaning their magnitude
 	# equal to the radius of circle!
-	var angle = a.dot(b) / (_inner_radius*_inner_radius)
+	var angle = a.dot(b) / (_inner_radius * _inner_radius)
 	var theta = acos(angle)
 	var arc_length = theta * _inner_radius
 	
@@ -35,7 +35,7 @@ func _rand_point_on_circle() -> Vector2:
 	
 	return p
 	
-func _get_radius_from_point(p: Vector2, r: float) -> float:
+func _get_radius_from_point(p: Vector2, _r: float) -> float:
 	var angle: float = atan2(p.y, p.x)
 	var angle_offset: float = angle / _inner_radius
 	angle += angle_offset
@@ -72,20 +72,23 @@ func try_gen_positions() -> void:
 			sorted_positions.push_back(gen_p)
 			continue
 			
-		var idx = sorted_positions.bsearch(gen_p)
-		if idx == 0:
-			if _test_positions(Vector2(gen_p.x, gen_p.y), Vector2(sorted_positions[idx].x, sorted_positions[idx].y)):
-				sorted_positions.insert(idx, gen_p)
-		elif idx == sorted_positions.size() - 1:
-			if _test_positions(Vector2(gen_p.x, gen_p.y), Vector2(sorted_positions[idx - 1].x, sorted_positions[idx - 1].y)):
-				sorted_positions.insert(idx, gen_p)
+		var j = sorted_positions.bsearch(gen_p)
+		if j == 0:
+			if _test_positions(Vector2(gen_p.x, gen_p.y), Vector2(sorted_positions[j].x, sorted_positions[j].y)):
+				sorted_positions.insert(j, gen_p)
+		elif j == sorted_positions.size() - 1:
+			if _test_positions(Vector2(gen_p.x, gen_p.y), Vector2(sorted_positions[j - 1].x, sorted_positions[j - 1].y)):
+				sorted_positions.insert(j, gen_p)
 		elif sorted_positions.size() > 3 and sorted_positions.size() < _max_obstacles - 1:
-			if _test_positions(Vector2(gen_p.x, gen_p.y), Vector2(sorted_positions[idx - 1].x, sorted_positions[idx - 1].y)) and \
-				_test_positions(Vector2(gen_p.x, gen_p.y), Vector2(sorted_positions[idx + 1].x, sorted_positions[idx + 1].y)):
-				sorted_positions.insert(idx, gen_p)
+			if _test_positions(Vector2(gen_p.x, gen_p.y), Vector2(sorted_positions[j - 1].x, sorted_positions[j - 1].y)) and \
+				_test_positions(Vector2(gen_p.x, gen_p.y), Vector2(sorted_positions[j + 1].x, sorted_positions[j + 1].y)):
+				sorted_positions.insert(j, gen_p)
 		else:
-			sorted_positions.insert(idx, gen_p)
+			sorted_positions.insert(j, gen_p)
 		
 		
 	#TODO: add logic to do probing on array and adjust gen_p to fit it somewhere 
 	# between neighbour elements instead ? 
+
+func _enemy_spawn() -> void:
+	pass
